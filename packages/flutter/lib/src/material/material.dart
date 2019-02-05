9f5695baf1339be776e1ab6c88d1ea3b3dc9e9f1
jslavitz <jslavitz@google.com>
2019-02-05 14:37:55 -0800

    changes

--- packages/flutter/lib/src/material/material.dart
+++ packages/flutter/lib/src/material/material.dart
@@ -516,6 +516,7 @@ class _MaterialState extends State<Material> with TickerProviderStateMixin {
     }
 
     final ShapeBorder shape = _getShape();
+    print(shape);
 
     if (widget.type == MaterialType.transparency) {
       return _transparentInterior(
